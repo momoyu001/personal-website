f8ef338f798343c456b25fcb98adfa69b6509792
momoyu001 <lixia1220x@163.com>
2023-02-19 16:14:17 +0800

    feat: 笔记文章 - 面试

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,7 +5,7 @@ export default {
         siteTitle: 'momoyu', // false 不展示左上角的网站标题；也可自定义网站标题；不配置该项时，会默认采用 config.title 的值
         nav: [
             // 链接的路径设置为不带 .md 前缀的实际文件，并始终以 / 开头。
-            { text: '面试', link: '/greeting' },
+            { text: '面试', link: '/interview/' },
             { text: '数据结构和算法', link: '' },
             { text: 'JavaScript', link: '' },
             { text: 'Vue', link: '' },
@@ -14,5 +14,11 @@ export default {
             { text: '日常随笔', link: '' },
             { text: '其他', link: '' },
         ],
+        sidebar: {
+            '/interview/': [
+                { text: '面试整理-按公司分类', link: '/interview/company' },
+                { text: '面试整理-按模块分类', link: '/interview/module' },
+            ],
+        },
     },
 };
