696e512c7dc351d1a329c195bce254703c89b57e
momoyu001 <lixia1220x@163.com>
2023-02-19 16:36:22 +0800

    feat: 添加介绍页 - 面试、数据结构和算法

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,8 +5,8 @@ export default {
         siteTitle: 'momoyu', // false 不展示左上角的网站标题；也可自定义网站标题；不配置该项时，会默认采用 config.title 的值
         nav: [
             // 链接的路径设置为不带 .md 前缀的实际文件，并始终以 / 开头。
-            { text: '面试', link: '/interview/company' },
-            { text: '数据结构和算法', link: '/algorithm/note' },
+            { text: '面试', link: '/interview/' },
+            { text: '数据结构和算法', link: '/algorithm/' },
             { text: 'JavaScript', link: '/javascript/' },
             { text: 'Vue', link: '' },
             { text: 'React', link: '' },
